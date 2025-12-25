378daeafc76adfdda2e2883500de113629b44f4e
yubo <yubo.yue@163.com>
2025-12-25 22:50:27 +0800

    user2 data

--- network.py
+++ network.py
@@ -94,7 +94,7 @@ def udp_ok_listener(ip, app):
 
 def udp_listener(ip, port, app):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    csv_writer = CSVCsiWriter(max_records = 10000, output_dir="data/user1_pos5_static")
+    csv_writer = CSVCsiWriter(max_records = 10000, output_dir="data/user2_pos5_standing")
     recv_cnt = 0
     report_cnt = 0
     last_time = time.time()
